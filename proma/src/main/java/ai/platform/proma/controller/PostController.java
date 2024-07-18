a8db45f08ef0e31d7701fbe961ca69d6b0300dc4
ansungwoo <asw4ever@naver.com>
2024-07-19 02:21:34 +0900

    feat: 스크랩 컨트롤러 구현

--- proma/src/main/java/ai/platform/proma/controller/PostController.java
+++ proma/src/main/java/ai/platform/proma/controller/PostController.java
@@ -3,16 +3,17 @@ package ai.platform.proma.controller;
 import ai.platform.proma.domain.enums.PromptCategory;
 import ai.platform.proma.dto.response.PageInfo;
 import ai.platform.proma.dto.response.PostResponseDto;
+import ai.platform.proma.dto.response.PromptResponseDto;
 import ai.platform.proma.dto.response.ResponseDto;
 import ai.platform.proma.service.PostService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,11 +21,12 @@ import java.util.Map;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+//@RequestMapping("/community")
 public class PostController {
 
     private final PostService postService;
 
-    @GetMapping("/posts/paging")
+    @GetMapping("/community")
     public ResponseDto<Map<String, Object>> getCommunityPosts(
             @RequestParam(value = "search", required = false) String searchKeyword,
             @RequestParam(value = "category", required = false) PromptCategory category,
@@ -43,5 +45,14 @@ public class PostController {
 
         return new ResponseDto<>(response);
     }
+
+    @GetMapping("/community/scrap/{postId}/") // JWT사용시 수정 필요
+    public ResponseDto<Boolean> postScrap(
+            @PathVariable("postId") Long postId,
+            @RequestParam(value = "userId", required = false) Long userId
+    ) {
+        return new ResponseDto<>(postService.scrapPrompt(postId, userId));
+    }
+
 
 }
