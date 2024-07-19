32fa60e6453c84c27e2d40b1b5442c2bacb8151c
park20011029 <rokmck156@gmail.com>
2024-07-19 23:11:39 +0900

    Feat: Add repository 연결

--- proma/src/main/java/ai/platform/proma/repositroy/CommunicationMethodRepository.java
+++ proma/src/main/java/ai/platform/proma/repositroy/CommunicationMethodRepository.java
@@ -2,9 +2,11 @@ package ai.platform.proma.repositroy;
 import ai.platform.proma.domain.CommunicationMethod;
 import ai.platform.proma.domain.enums.PromptType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CommunicationMethodRepository extends JpaRepository<CommunicationMethod, Long> {
     Optional<CommunicationMethod> findByType(PromptType type);
 }
