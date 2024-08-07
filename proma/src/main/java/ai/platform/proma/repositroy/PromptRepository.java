47cf35df73aaa695e5e04330cdf5a2537942641b
ansungwoo <asw4ever@naver.com>
2024-08-07 16:08:57 +0900

    fix: Scrap enums에 SHARED 추가

--- proma/src/main/java/ai/platform/proma/repositroy/PromptRepository.java
+++ proma/src/main/java/ai/platform/proma/repositroy/PromptRepository.java
@@ -1,12 +1,10 @@
 package ai.platform.proma.repositroy;
 
 import ai.platform.proma.domain.Prompt;
-import ai.platform.proma.domain.PromptMethods;
 import ai.platform.proma.domain.User;
-import ai.platform.proma.domain.enums.Scrap;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,5 +13,6 @@ public interface PromptRepository extends JpaRepository<Prompt, Long> {
 
     Optional<Prompt> findByIdAndUser(Long promptId, User user);
 
-    List<Prompt> findByUser(User user);
+    @Query("SELECT p FROM Prompt p WHERE p.user = :user AND p.isScrap IN (ai.platform.proma.domain.enums.Scrap.SCRAP, ai.platform.proma.domain.enums.Scrap.NOTSCRAP)")
+    List<Prompt> findByUserAndScrap(@Param("user") User user);
 }
