acea7f955d3189b8b88930099204708a73865ccf
park20011029 <rokmck156@gmail.com>
2024-08-10 16:19:19 +0900

    Feat: enum 영어버전 추가

--- proma/src/main/java/ai/platform/proma/domain/enums/BlockCategory.java
+++ proma/src/main/java/ai/platform/proma/domain/enums/BlockCategory.java
@@ -3,40 +3,58 @@ package ai.platform.proma.domain.enums;
 import ai.platform.proma.exception.ApiException;
 import ai.platform.proma.exception.ErrorDefine;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.i18n.LocaleContextHolder;
 
 import java.util.Arrays;
+import java.util.Locale;
 
-@RequiredArgsConstructor
+
 public enum BlockCategory {
+
     // 화자, 청자, 지시, 형식, 참고자료, 필수, 제외, 역할, 성격, 배경지식, 규칙
-    SPEAKER("화자"),  // 화자
-    LISTENER("청자"), // 청자
-    INSTRUCTION("지시"),  // 지시
-    FORM("형식"),  // 형식
-    REFERENCE("참고자료"),  // 참고자료
-    REQUIRED("필수"),  // 필수
-    EXCLUDED("제외"),  // 제외
-    ROLE("역할"), //역할
-    CHARACTER("성격"), // 성격
-    BACKGROUND_KNOWLEDGE("배경지식"), // 배경지식
-    RULE("규칙"), // 규칙
-    FREE("자유"); // 자유형 규격
+    SPEAKER("화자","Speaker"),  // 화자
+    LISTENER("청자","Listener"), // 청자
+    INSTRUCTION("지시","Instruction"),  // 지시
+    FORM("형식","Form"),  // 형식
+    REFERENCE("참고자료","Reference"),  // 참고자료
+    REQUIRED("필수","Required"),  // 필수
+    EXCLUDED("제외","Excluded"),  // 제외
+    ROLE("역할","Role"), //역할
+    CHARACTER("성격","Character"), // 성격
+    BACKGROUND_KNOWLEDGE("배경지식","Background"), // 배경지식
+    RULE("규칙","Rule"), // 규칙
+    FREE("자유","Free"); // 자유형 규격
 
-    private final String displayName;
+    private final String msgKo;
+    private final String msgEn;
 
-    public static BlockCategory fromValue(String value) {
-        return Arrays.stream(BlockCategory.values())
-                .filter(v -> v.displayName.equalsIgnoreCase(value))
-                .findFirst()
-                .orElse(null);
+    private BlockCategory(String msgKo, String msgEn) {
+        this.msgKo = msgKo;
+        this.msgEn = msgEn;
     }
 
     @Override
-    public String toString() {
-        return displayName;
+    public String toString(){
+        Locale locale = LocaleContextHolder.getLocale();
+        // 한국어: ko, 영어: en, 일본어: ja
+        if("en".equals(locale.getLanguage()))
+            return this.msgEn;
+        else
+            return this.msgKo;
+
     }
 
-
-
-
+    public static BlockCategory fromValue(String value) {
+        Locale locale = LocaleContextHolder.getLocale();
+        if("en".equals(locale.getLanguage()))
+            return Arrays.stream(BlockCategory.values())
+                    .filter(v -> v.msgEn.equalsIgnoreCase(value))
+                    .findFirst()
+                    .orElse(null);
+        else
+            return Arrays.stream(BlockCategory.values())
+                    .filter(v -> v.msgKo.equalsIgnoreCase(value))
+                    .findFirst()
+                    .orElse(null);
+    }
 }
