d7521b6a5cbca2e5ee20de249e3babd360b945ae
ansungwoo <asw4ever@naver.com>
2024-09-30 19:58:50 +0900

    fix: repository 어노테이션 추가

--- proma/src/main/java/ai/platform/proma/repositroy/LikeRepository.java
+++ proma/src/main/java/ai/platform/proma/repositroy/LikeRepository.java
@@ -7,8 +7,10 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+@Repository
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
     int countByPostId(Long postId);
@@ -18,5 +20,6 @@ public interface LikeRepository extends JpaRepository<Like, Long> {
     List<Long> findPostIdsByUserId(@Param("userId") Long userId);
 
     boolean existsByPost(Post post); // 로그인 안한 사용자
-
+    @Query("SELECT l.post.id FROM Like l WHERE l.post IN :posts AND l.user = :user")
+    List<Long> findLikedPostIdsByPostsAndUser(@Param("posts") List<Post> posts, @Param("user") User user);
 }
--- proma/src/main/java/ai/platform/proma/repositroy/MessageRepository.java
+++ proma/src/main/java/ai/platform/proma/repositroy/MessageRepository.java
@@ -3,8 +3,10 @@ package ai.platform.proma.repositroy;
 import ai.platform.proma.domain.ChatRoom;
 import ai.platform.proma.domain.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+@Repository
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
