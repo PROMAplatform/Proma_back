d7041514ebfb27285e30b8c718b79d088ba12490
park20011029 <rokmck156@gmail.com>
2024-10-04 19:52:00 +0900

    Fix: 리펙토링 과정중 오류 수정

--- proma/src/main/java/ai/platform/proma/service/query/post/PostDistributePromptService.java
+++ proma/src/main/java/ai/platform/proma/service/query/post/PostDistributePromptService.java
@@ -17,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 @RequiredArgsConstructor
 public class PostDistributePromptService implements PostDistributePromptUseCase {
 
