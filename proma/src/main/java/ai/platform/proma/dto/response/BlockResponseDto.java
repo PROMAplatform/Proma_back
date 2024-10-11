14d78f2fc71c29550e2ae41444b98f9a1c3d04ff
ansungwoo <asw4ever@naver.com>
2024-10-11 20:36:40 +0900

    bug: ResponseDTO 메서드 버그 수정

--- proma/src/main/java/ai/platform/proma/dto/response/BlockResponseDto.java
+++ proma/src/main/java/ai/platform/proma/dto/response/BlockResponseDto.java
@@ -19,6 +19,7 @@ public class BlockResponseDto {
         return BlockResponseDto.builder()
                 .blockId(block.getId())
                 .blockValue(block.getBlockValue())
+                .blockCategory(block.getBlockValue())
                 .build();
     }
 }
